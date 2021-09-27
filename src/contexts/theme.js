f95f63b30c9019f17de7d86a8db3671666fab422
Elijah Obominuru <elijahobominuru@gmail.com>
2021-09-27 06:50:09 +0100

    add sample toggle effect for light and dark mode

---
+++ src/contexts/theme.js
@@ -0,0 +1,36 @@
+import { createContext, useEffect, useState } from "react";
+
+const themes = {
+  dark: {
+    background: "black",
+    color: "white",
+  },
+  light: {
+    background: "white",
+    color: "black",
+  },
+};
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState(false);
+
+  const theme = isDark ? themes.dark : themes.light;
+
+  const toggleTheme = () => {
+      localStorage.setItem("isDark", JSON.stringify(!isDark))
+    setIsDark(!isDark);
+  };
+
+  useEffect(() => {
+      const isDark = localStorage.getItem("isDark") === "true"
+      setIsDark(isDark)
+  }, [])
+
+  return (
+    <ThemeContext.Provider value={[{ theme, isDark }, toggleTheme]}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
--- src/views/dashboard/layout/DashboardLayout.js
+++ src/views/dashboard/layout/DashboardLayout.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import SideBar from "../../../components/sideBar/SideBar";
+import { ThemeContext } from "../../../contexts/theme";
+import TopNav from "../../../components/topNav/TopNav"
 import "./DashboardLayout.css";
 const DashboardLayout = (props) => {
+  const [{ theme, isDark }, toggleTheme] = useContext(ThemeContext);
+  console.log("theme", theme);
   return (
-    <div className='wrap'>
+    <div
+      className='wrap'
+      style={{ background: theme.background, color: theme.color }}
+    >
       <div className='left'>
         <SideBar />
       </div>
 
       <div className='right'>
+        <TopNav />
+        <button onClick={toggleTheme}>toggle btn</button>
         <div className=''>{props.children}</div>
       </div>
     </div>
