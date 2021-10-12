0143da1ccc343ae93e0792d2af97af433469d8e0
Elijah Obominuru <elijahobominuru@gmail.com>
2021-10-12 05:11:49 +0100

    add course component

--- src/views/dashboard/dashboardCourses/DashboardCourses.js
+++ src/views/dashboard/dashboardCourses/DashboardCourses.js
@@ -1,11 +1,112 @@
-import React from 'react'
+import React from "react";
+import SearchFilter from "../../../components/searchFilter/SearchFilter";
+import one from "../../../assets/images/Group 35.png";
+import "./DashboardCourses.css";
+import { BiDotsVerticalRounded } from "react-icons/bi";
 
 const DashboardCourses = () => {
-    return (
-        <div>
-            Courses
-        </div>
-    )
-}
+  return (
+    <div className='courses_wrapper'>
+      <h1>Courses</h1>
 
-export default DashboardCourses
+      <div className='search_filter_wrapper'>
+        <SearchFilter />
+      </div>
+
+      <div className='courses_module'>
+        <div className='single_courses'>
+          <div className='courses_cta'>
+            <input type='checkbox' name='' id='' />
+            <span>
+              <BiDotsVerticalRounded />
+            </span>
+          </div>
+          <div className='course_content_wrapper'>
+            <img src={one} alt='' />
+            <h3>Amazon Web Services</h3>
+            <p>
+              <span>2108 </span> <span className="special_letter">Videos</span>
+            </p>
+          </div>
+        </div>
+        <div className='single_courses'>
+        <div className='courses_cta'>
+            <input type='checkbox' name='' id='' />
+            <span>
+              <BiDotsVerticalRounded />
+            </span>
+          </div>
+          <div className='course_content_wrapper'>
+            <img src={one} alt='' />
+            <h3>Amazon Web Services</h3>
+            <p>
+              <span>2108 </span> <span className="special_letter">Videos</span>
+            </p>
+          </div>
+        </div>
+        <div className='single_courses'>
+        <div className='courses_cta'>
+            <input type='checkbox' name='' id='' />
+            <span>
+              <BiDotsVerticalRounded />
+            </span>
+          </div>
+          <div className='course_content_wrapper'>
+            <img src={one} alt='' />
+            <h3>Amazon Web Services</h3>
+            <p>
+              <span>2108 </span> <span className="special_letter">Videos</span>
+            </p>
+          </div>
+        </div>
+        <div className='single_courses'>
+        <div className='courses_cta'>
+            <input type='checkbox' name='' id='' />
+            <span>
+              <BiDotsVerticalRounded />
+            </span>
+          </div>
+          <div className='course_content_wrapper'>
+            <img src={one} alt='' />
+            <h3>Amazon Web Services</h3>
+            <p>
+              <span>2108 </span> <span className="special_letter">Videos</span>
+            </p>
+          </div>
+        </div>
+        <div className='single_courses'>
+        <div className='courses_cta'>
+            <input type='checkbox' name='' id='' />
+            <span>
+              <BiDotsVerticalRounded />
+            </span>
+          </div>
+          <div className='course_content_wrapper'>
+            <img src={one} alt='' />
+            <h3>Amazon Web Services</h3>
+            <p>
+              <span>2108 </span> <span className="special_letter">Videos</span>
+            </p>
+          </div>
+        </div>
+        <div className='single_courses'>
+        <div className='courses_cta'>
+            <input type='checkbox' name='' id='' />
+            <span>
+              <BiDotsVerticalRounded />
+            </span>
+          </div>
+          <div className='course_content_wrapper'>
+            <img src={one} alt='' />
+            <h3>Amazon Web Services</h3>
+            <p>
+              <span>2108 </span> <span className="special_letter">Videos</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardCourses;
