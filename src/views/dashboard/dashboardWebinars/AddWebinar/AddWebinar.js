25e1bb4dfda7d5acb5f78bf4d85f678c0e57eff7
Elijah Obominuru <elijahobominuru@gmail.com>
2021-10-29 02:38:20 +0100

    add editor

--- src/views/dashboard/dashboardWebinars/AddWebinar/AddWebinar.js
+++ src/views/dashboard/dashboardWebinars/AddWebinar/AddWebinar.js
@@ -1,8 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./AddWebinar.scss";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    background: "#212121",
+    color: "rgba(244, 244, 244, 0.8)",
+    border: "none",
+    marginTop: "12px",
+
+    "& .ql-container": {
+      minHeight: "300px",
+    },
+
+    "& .ql-container.ql-snow": {
+      border: "none",
+    },
+
+    "& .ql-toolbar.ql-snow": {
+      border: "0.4px solid rgba(107, 107, 107, 0.8)",
+      boxSizing: "border-box",
+      padding: "8px",
+      color: "rgba(244, 244, 244, 0.8)",
+    },
+  },
+});
 
 const AddWebinar = () => {
+  const classes = useStyles();
+  const [value, setValue] = useState("");
+  console.log("value", value);
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
   return (
     <div className='add_career_wrapper'>
       <div className='add_career_redirect'>
@@ -42,31 +102,53 @@ const AddWebinar = () => {
                 </div>
               </div>
               <div className='add_career_role__form-group'>
-                <label className='add_career_role__form-label'>
-                  Description
-                </label>
-                <textarea
-                  rows='8'
-                  className='add_career_role__form-input'
-                ></textarea>
+                <label className='add_career_role__form-label'>Comments</label>
+                <ReactQuill
+                  theme='snow'
+                  className={classes.root}
+                  value={value}
+                  onChange={setValue}
+                  modules={modules}
+                  formats={formats}
+                />
               </div>
+
+              <div className='add_career_role__form-group'>
+                <label className='add_career_role__form-label'>Comments</label>
+                <ReactQuill
+                  theme='snow'
+                  className={classes.root}
+                  value={value}
+                  onChange={setValue}
+                  modules={modules}
+                  formats={formats}
+                />
+              </div>
+              
+              <h4 className='add_career_role__select--heading'>
+              Media Settings
+              </h4>
+
               <div className='add_career_role__form-group'>
                 <label className='add_career_role__form-label'>
-                  Resposibility
+                  Image Settings
                 </label>
-                <textarea
-                  rows='8'
-                  className='add_career_role__form-input'
-                ></textarea>
+                <input type='text' className='add_career_role__form-input' />
               </div>
               <div className='add_career_role__form-group'>
+                <label className='add_career_role__form-label'>Alt</label>
+                <input type='text' className='add_career_role__form-input' />
+              </div>
+
+              <div className='add_career_role__form-group'>
                 <label className='add_career_role__form-label'>
-                  Qualification
+                  Image Settings
                 </label>
-                <textarea
-                  rows='8'
-                  className='add_career_role__form-input'
-                ></textarea>
+                <input type='text' className='add_career_role__form-input' />
+              </div>
+              <div className='add_career_role__form-group'>
+                <label className='add_career_role__form-label'>Alt</label>
+                <input type='text' className='add_career_role__form-input' />
               </div>
             </div>
           </div>
